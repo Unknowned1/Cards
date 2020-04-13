7806d4b5d8d6521a85b459f9e5029dc7ece57638
Alex <bacco.a10@gmail.com>
2020-04-13 22:21:47 +0200

    fixed timeeeeeer
    fu timer

---
+++ Assets/script/TurnTimer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TurnTimer : MonoBehaviour
+{
+    public Text counterText;
+
+    public float seconds, minutes, startTime;
+
+    // Initialization
+    void Start()
+    {
+        counterText = GetComponent<Text>() as Text;
+        startTime = Time.time;
+        seconds = 0;
+    }
+
+    // Update once per frame
+    void Update()
+    {
+        if (seconds <= 10f)
+        {
+            seconds += 1 * Time.deltaTime;
+            counterText.text = string.Format("{0:00}:{1:00}", minutes, seconds);  
+        }
+        if (seconds >= 10f)
+        {
+            minutes = 0;
+            seconds = 0;
+            counterText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        }
+    }
+}
--- Assets/script/timer_handler.cs
+++ Assets/script/timer_handler.cs
@@ -12,7 +12,7 @@ public class timer_handler : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        time = 10;
+        time = 180;
         timer = GetComponent<Text>();
         timer_text = timer.text;
         
